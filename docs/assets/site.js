86d6d6e5d26310e0852bc388f7043a0adf8e580c
加納寛之 <kanouhiroyuki@MacBook-Air.local>
2026-09-18 08:37:09 +0900

    chore: APIリクエスト先を ngrok URL に更新 ()

--- docs/assets/site.js
+++ docs/assets/site.js
@@ -158,7 +158,7 @@ function initAttendanceForm() {
   const form = $("#attendance-form");
   if (!form) return;
 
-  const API_URL = "https://629c-240b-c020-651-26dc-c0a8-54eb-158b-29b1.ngrok-free.app/api/v1/attendances";
+  const API_URL = "https://2a45-240b-c020-651-26dc-ed8f-9df7-6fb0-b5e9.ngrok-free.app/api/v1/attendances";
 
   const { year, subject, className } = getParams();
   const lessonCode =
